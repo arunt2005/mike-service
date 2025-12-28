022ae2649e6ad293b9a5e5c55e0cffd39c5563a9
arunt2005 <arunt2005@gmail.com>
2025-12-28 21:36:48 +0530

    first commit

---
+++ src/test/java/com/example/MikeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
